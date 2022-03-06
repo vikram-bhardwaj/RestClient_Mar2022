4d5e0a02fc5d7fa959849a4d4c83ad7c9689f016
vikram-bhardwaj <vikrambhardwaj94@gmail.com>
2022-03-06 06:32:33 +0530

    adding JSON parshing

--- src/main/java/com/test/ClientTest.java
+++ src/main/java/com/test/ClientTest.java
@@ -7,12 +7,15 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ClientTest
 {
 
 	public static void main(String[] args)
 	{
+		String result = "";
 		try
 		{
 			DefaultHttpClient httpClient = new DefaultHttpClient();
@@ -30,7 +33,38 @@ public class ClientTest
 			System.out.println("Output from Server .... \n");
 			while ((output = br.readLine()) != null)
 			{
-				System.out.println(output);
+				//System.out.println(output);
+				result = result.concat(output);
+			}
+			
+			JSONObject jObject = new JSONObject(result);
+			JSONArray empData = jObject.getJSONObject("details").getJSONArray("empDetails");
+			int empLen = empData.length();
+			for(int i = 0; i < empLen; i++)
+			{
+				String iterateData = empData.get(i).toString();
+				JSONObject temp = new JSONObject(iterateData);
+				String id = temp.getString("ID");
+				String name = temp.getString("name");
+				String age = temp.getString("age");
+				String gender = temp.getString("gender");
+				String address = temp.getString("address");
+				String phonenumber = temp.getString("phonenumber");
+				
+				System.out.println(id + " | " + name + " | " + age + " | " + gender + " | " + address + " | " + phonenumber);
+			}
+			System.out.println("______________________________________________________________________");
+			JSONArray compData = jObject.getJSONObject("details").getJSONArray("compDetails");
+			int compLen = compData.length();
+			for(int i = 0; i < compLen; i++)
+			{
+				String iterateData = compData.get(i).toString();
+				JSONObject temp = new JSONObject(iterateData);
+				String companyName = temp.getString("companyName");
+				String companyLocation = temp.getString("companyLocation");
+				String companyType = temp.getString("companyType");
+				
+				System.out.println(companyName + " | " + companyLocation + " | " + companyType);
 			}
 			
 			httpClient.getConnectionManager().shutdown();
